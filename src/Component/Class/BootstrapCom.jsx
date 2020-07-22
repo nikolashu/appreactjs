de699f83179774809754be0f5b13e6152b085650
Nikolas Setiawan <nikolashu16@gmail.com>
2020-07-22 15:08:57 +0700

    menambahkan bootstrap

---
+++ src/Component/Class/BootstrapCom.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class BootstrapCom extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                Hello Bootstrap
+            </div>
+        )
+    }
+}
+
+export default BootstrapCom
